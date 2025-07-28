58d23f3bd4f575c6d69c0f742de557fef8639cf8
Quynh-Anh Nguyen <57626383+ococ22@users.noreply.github.com>
2025-07-28 14:46:21 +0700

    Create Squeeze.py

---
+++ Squeeze.py
@@ -0,0 +1,12 @@
+class SqueezeLayer(Layer):
+    def __init__(self, axis, **kwargs):
+        super(SqueezeLayer, self).__init__(**kwargs)
+        self.axis = axis
+
+    def call(self, inputs):
+        return tf.squeeze(inputs, axis=self.axis)
+
+    def get_config(self):
+        config = super(SqueezeLayer, self).get_config()
+        config.update({"axis": self.axis})
+        return config
